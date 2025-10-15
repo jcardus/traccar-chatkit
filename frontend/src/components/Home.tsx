6f00589211fa020371019c5b97901b6d9f603433
Joaquim Cardeira <joaquim_cardeira@hotmail.com>
2025-10-15 01:38:50 +0100

    fix toggle

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,7 +1,6 @@
 import clsx from "clsx";
 
 import { ChatKitPanel } from "./ChatKitPanel";
-import { FactCard } from "./FactCard";
 import { ThemeToggle } from "./ThemeToggle";
 import { ColorScheme } from "../hooks/useColorScheme";
 import { useFacts } from "../hooks/useFacts";
@@ -13,7 +12,7 @@ export default function Home({
   scheme: ColorScheme;
   handleThemeChange: (scheme: ColorScheme) => void;
 }) {
-  const { facts, refresh, performAction } = useFacts();
+  const { refresh, performAction } = useFacts();
 
   const containerClass = clsx(
     "min-h-screen bg-gradient-to-br transition-colors duration-300",
@@ -26,15 +25,15 @@ export default function Home({
     <div className={containerClass}>
       <div className="mx-auto flex min-h-screen w-full max-w-6xl flex-col-reverse gap-10 px-6 pt-4 pb-10 md:py-10 lg:flex-row">
         <div className="relative w-full flex h-[90vh] items-stretch overflow-hidden rounded-3xl bg-white/80 shadow-[0_45px_90px_-45px_rgba(15,23,42,0.6)] ring-1 ring-slate-200/60 backdrop-blur md:h-[90vh] dark:bg-slate-900/70 dark:shadow-[0_45px_90px_-45px_rgba(15,23,42,0.85)] dark:ring-slate-800/60">
+          <div className="absolute z-40 p-3">
+            <ThemeToggle value={scheme} onChange={handleThemeChange}  />
+          </div>
           <ChatKitPanel
             theme={scheme}
             onWidgetAction={performAction}
             onResponseEnd={refresh}
             onThemeRequest={handleThemeChange}
           />
-        </div>
-        <div className="">
-          <ThemeToggle value={scheme} onChange={handleThemeChange}  />
         </div>
       </div>
     </div>
