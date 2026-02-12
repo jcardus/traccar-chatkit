f24507793c49da75f11a9fb797965e9cdbe8ea99
jcardeira <joaquim.cardeira@gmail.com>
2026-02-12 00:13:27 +0000

    log

--- frontend/functions/chatkit.js
+++ frontend/functions/chatkit.js
@@ -1,5 +1,6 @@
 export const onRequest = ({request, env}, cf) => {
     const url = new URL(request.url)
     url.host = env.CHAT_SERVER || 'localhost'
+    console.log(url)
     return fetch(new Request(url, request), cf)
 }
