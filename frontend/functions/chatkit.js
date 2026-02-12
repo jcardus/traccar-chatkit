e9fae48800e73c3ea5a07e7bf8feae301b874980
jcardeira <joaquim.cardeira@gmail.com>
2026-02-12 00:05:28 +0000

    fix proxy

--- frontend/functions/chatkit.js
+++ frontend/functions/chatkit.js
@@ -1,7 +1,5 @@
 export const onRequest = ({request, env}, cf) => {
     const url = new URL(request.url)
     url.host = env.CHAT_SERVER || 'localhost'
-    url.protocol = 'http:'
-    url.port = 8000
     return fetch(new Request(url, request), cf)
 }
